f6693d7a290709670f950f8b300cfbb0c6d85cf8
tal-amir <tal.amir.99@gmail.com>
2025-06-08 16:59:30 +0300

    Working on

--- src/fswlib/__init__.py
+++ src/fswlib/__init__.py
@@ -1,4 +1,8 @@
 # src/fswlib/__init__.py
+"""
+.. include:: ../../README.md
+"""
+
 from .fsw_embedding.fsw_embedding import FSWEmbedding, FSWCustomCudaExtensionLoadWarning, FSWCustomCudaExtensionLoadError
 from .fsw_embedding.test_fsw_embedding import main as test_fsw_embedding
 
