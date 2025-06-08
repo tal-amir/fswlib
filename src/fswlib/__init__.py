a544b6186a122ee7f288a5867acd6c695ee76da8
tal-amir <tal.amir.99@gmail.com>
2025-06-08 17:44:59 +0300

    PyPI v0.9.2

--- src/fswlib/__init__.py
+++ src/fswlib/__init__.py
@@ -3,7 +3,13 @@
 .. include:: ../../README.md
 """
 
-from .fsw_embedding.fsw_embedding import FSWEmbedding, FSWCustomCudaExtensionLoadWarning, FSWCustomCudaExtensionLoadError
+from .fsw_embedding.fsw_embedding import (FSWEmbedding,
+                                          TotalMassEncodingTransformation,
+                                          TotalMassEncodingMethod,
+                                          FrequencyInitMethod,
+                                          FSWCustomCudaExtensionLoadWarning,
+                                          FSWCustomCudaExtensionLoadError)
+
 from .fsw_embedding.test_fsw_embedding import main as test_fsw_embedding
 
-__all__ = ["FSWEmbedding", "FSWCustomCudaExtensionLoadWarning", "FSWCustomCudaExtensionLoadError", "test_fsw_embedding"]
+__all__ = ["FSWEmbedding", "FSWCustomCudaExtensionLoadWarning", "FSWCustomCudaExtensionLoadError", "TotalMassEncodingTransformation", "TotalMassEncodingMethod", "FrequencyInitMethod", "test_fsw_embedding"]
